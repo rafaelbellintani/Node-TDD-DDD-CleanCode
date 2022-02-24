b92d58d60c43cdafd7ea21b5ac7c5df66b21dde6
Rafael Bellintani <rafaelbellintani@outlook.com>
2022-02-24 16:45:41 +0044

    test: add new tests for password and password confirmation

--- src/tests/signup.spec.ts
+++ src/tests/signup.spec.ts
@@ -32,3 +32,35 @@ test('signup with bad email and expect 400 with param missing', () => {
   expect(httpResponse.statusCode).toBe(400)
   expect(httpResponse.body).toEqual(new MissingParamsError('email'))
 })
+
+test('signup with bad password and expect 400 with param missing', () => {
+  const sut = new SignupController()
+
+  const httpRequest = {
+    body: {
+      username: 'any',
+      email: 'any@example.com',
+      passwordConfirmation: 'any_password'
+    }
+  }
+
+  const httpResponse = sut.handle(httpRequest)
+  expect(httpResponse.statusCode).toBe(400)
+  expect(httpResponse.body).toEqual(new MissingParamsError('password'))
+})
+
+test('signup with bad passwordConfirmation and expect 400 with param missing', () => {
+  const sut = new SignupController()
+
+  const httpRequest = {
+    body: {
+      username: 'any',
+      email: 'any@example.com',
+      password: 'any_password'
+    }
+  }
+
+  const httpResponse = sut.handle(httpRequest)
+  expect(httpResponse.statusCode).toBe(400)
+  expect(httpResponse.body).toEqual(new MissingParamsError('passwordConfirmation'))
+})
