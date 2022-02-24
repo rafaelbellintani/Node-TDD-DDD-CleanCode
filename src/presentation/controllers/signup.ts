df59eae4e73c244928585464e43603d994860254
Rafael Bellintani <rafaelbellintani@outlook.com>
2022-02-24 12:25:35 +0044

    refactor: add helper badRequest return HttpResponse

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,14 @@
 import { MissingParamsError } from '../errors/missing-params'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { Controller } from '../protocols/controller'
+import { badRequest } from '../helpers/http-helper'
 
 export class SignupController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const fields = ['username', 'email', 'password', 'passwordConfirmation']
     for (const field of fields) {
       if (!httpRequest.body[field]) {
-        return ({
-          statusCode: 400,
-          body: new MissingParamsError(field)
-        })
+        return badRequest(new MissingParamsError(field))
       }
     }
     throw new Error('none')
