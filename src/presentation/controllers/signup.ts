288eff545aaeaf4aab617837df2ca329d828e1ca
Rafael Bellintani <rafaelbellintani@outlook.com>
2022-02-24 11:20:19 +0044

    refactor: add httprequest and httpresponse interface

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,8 @@
 import { MissingParamsError } from '../errors/missing-params'
+import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignupController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     const fields = ['username', 'email', 'password', 'passwordConfirmation']
     for (const field of fields) {
       if (!httpRequest.body[field]) {
