235a0fc54d780525f3a30d6b44016f372d7f0b3c
Rafael <rafaelbellintani@outlook.com>
2022-02-24 22:56:55 -0300

    test: signup with invalid email

--- src/tests/signup.spec.ts
+++ src/tests/signup.spec.ts
@@ -1,9 +1,20 @@
 import { SignupController } from '../presentation/controllers/signup'
 import { MissingParamsError } from '../presentation/errors/missing-params'
+import { EmailValidator } from '../presentation/protocols/email-validator'
+
+const makeSut = (): SignupController => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  const emailValidatorStub = new EmailValidatorStub()
+  return new SignupController(emailValidatorStub)
+}
 
 test('signup with bad username and expect 400 with param missing', () => {
-  const sut = new SignupController()
-
+  const sut = makeSut()
   const httpRequest = {
     body: {
       email: 'any@example.com',
@@ -18,7 +29,7 @@ test('signup with bad username and expect 400 with param missing', () => {
 })
 
 test('signup with bad email and expect 400 with param missing', () => {
-  const sut = new SignupController()
+  const sut = makeSut()
 
   const httpRequest = {
     body: {
@@ -34,7 +45,7 @@ test('signup with bad email and expect 400 with param missing', () => {
 })
 
 test('signup with bad password and expect 400 with param missing', () => {
-  const sut = new SignupController()
+  const sut = makeSut()
 
   const httpRequest = {
     body: {
@@ -50,7 +61,7 @@ test('signup with bad password and expect 400 with param missing', () => {
 })
 
 test('signup with bad passwordConfirmation and expect 400 with param missing', () => {
-  const sut = new SignupController()
+  const sut = makeSut()
 
   const httpRequest = {
     body: {
@@ -63,4 +74,21 @@ test('signup with bad passwordConfirmation and expect 400 with param missing', (
   const httpResponse = sut.handle(httpRequest)
   expect(httpResponse.statusCode).toBe(400)
   expect(httpResponse.body).toEqual(new MissingParamsError('passwordConfirmation'))
+})
+
+test('signup with bad email and expect 400 with param missing', () => {
+  const sut = makeSut()
+
+  const httpRequest = {
+    body: {
+      username: 'any',
+      email: 'any@example.com',
+      password: 'any_password',
+      passwordConfirmation: 'any_password'
+    }
+  }
+
+  const httpResponse = sut.handle(httpRequest)
+  expect(httpResponse.statusCode).toBe(400)
+  expect(httpResponse.body).toEqual(new Error('email'))
 })
