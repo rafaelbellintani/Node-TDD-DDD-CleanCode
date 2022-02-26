992ae134a062ee44b35dcc55fc77e10470c270b0
Rafael <rafaelbellintani@outlook.com>
2022-02-25 23:16:25 -0300

    test: ensure SignupController returns 400 if password confirmation fail

--- src/tests/signup.spec.ts
+++ src/tests/signup.spec.ts
@@ -108,6 +108,23 @@ test('signup with bad email and expect 400 with param missing', () => {
   expect(httpResponse.body).toEqual(new InvalidParamsError('email'))
 })
 
+test('Should return 400 if password is not equal passwordConfirmation', () => {
+  const { sut } = makeSut()
+
+  const httpRequest = {
+    body: {
+      username: 'any',
+      email: 'any@example.com',
+      password: 'invalid_password',
+      passwordConfirmation: 'any_password'
+    }
+  }
+
+  const httpResponse = sut.handle(httpRequest)
+  expect(httpResponse.statusCode).toBe(400)
+  expect(httpResponse.body).toEqual(new InvalidParamsError('passwordConfirmationError'))
+})
+
 test('Should pass with correct email account', () => {
   const { sut, emailValidatorStub } = makeSut()
   const emailSpyOn = jest.spyOn(emailValidatorStub, 'isValid')
